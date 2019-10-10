89258cddc4be3c8eb648058485ab55f920d3d980
Thomas Berrens <thomas.berrens@me.com>
2019-10-10 11:29:19 +0200

    test

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -6,7 +6,7 @@ namespace programmeren
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("tgtgd!");
         }
     }
 }
